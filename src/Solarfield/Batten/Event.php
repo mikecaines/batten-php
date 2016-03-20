15ff0cb819c9453853bd80431142513245a3b651
mike <mikecaines@gmail.com>
2016-03-20 18:45:16 -0300

    remove getRelatedTarget() from Event and EventInterface

--- src/Solarfield/Batten/Event.php
+++ src/Solarfield/Batten/Event.php
@@ -4,7 +4,6 @@ namespace Solarfield\Batten;
 class Event implements EventInterface {
 	private $type;
 	private $target;
-	private $relatedTarget;
 
 	public function getType() {
 		return $this->type;
@@ -14,13 +13,8 @@ class Event implements EventInterface {
 		return $this->target;
 	}
 
-	public function getRelatedTarget() {
-		return $this->relatedTarget;
-	}
-
 	public function __construct($aType, $aInfo = []) {
 		$this->type = (string)$aType;
 		$this->target = array_key_exists('target', $aInfo) ? $aInfo['target'] : null;
-		$this->relatedTarget = array_key_exists('relatedTarget', $aInfo) ? $aInfo['relatedTarget'] : null;
 	}
 }
--- src/Solarfield/Batten/EventInterface.php
+++ src/Solarfield/Batten/EventInterface.php
@@ -4,5 +4,4 @@ namespace Solarfield\Batten;
 interface EventInterface {
 	public function getType();
 	public function getTarget();
-	public function getRelatedTarget();
 }
