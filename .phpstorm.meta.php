26f8e440e4ca76fca346f1f6c7eb27ed58b99cf4
mike <mikecaines@gmail.com>
2016-04-23 14:42:56 -0300

    add .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+ */
+
+namespace PHPSTORM_META {
+	$STATIC_METHOD_TYPES = [
+		\Solarfield\Batten\ControllerPlugins::getByClass('') => [
+			"" == "@",
+		],
+
+		\Solarfield\Batten\ViewPlugins::getByClass('') => [
+			"" == "@",
+		],
+	];
+}
