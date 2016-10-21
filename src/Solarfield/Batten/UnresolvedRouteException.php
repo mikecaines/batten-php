99e2ed5e9f9e9efac6f215c9c3034e945aefa53f
mike <mikecaines@gmail.com>
2016-10-21 15:26:39 -0300

    implement __debugInfo() for some Exception classes

--- src/Solarfield/Batten/UnresolvedRouteException.php
+++ src/Solarfield/Batten/UnresolvedRouteException.php
@@ -10,6 +10,18 @@ class UnresolvedRouteException extends \Exception {
 		return $this->routeInfo;
 	}
 
+	public function __debugInfo() {
+		return [
+			'message' => $this->getMessage(),
+			'code' => $this->getCode(),
+			'file' => $this->getFile(),
+			'line' => $this->getLine(),
+			'trace' => $this->getTrace(),
+			'previous' => $this->getPrevious(),
+			'routeInfo' => $this->getRouteInfo(),
+		];
+	}
+
 	public function __construct($message = null, $code = 0, Exception $previous = null, $aRouteInfo = null) {
 		parent::__construct($message, $code, $previous);
 		$this->routeInfo = $aRouteInfo;
