754ee6681069796409680e97680e76a54a3df330
mike <mikecaines@gmail.com>
2016-10-21 15:24:36 -0300

    add UnresolvedRouteException::getRouteInfo() and related constructor parameter, to hold details about the route

--- src/Solarfield/Batten/UnresolvedRouteException.php
+++ src/Solarfield/Batten/UnresolvedRouteException.php
@@ -1,6 +1,17 @@
 <?php
 namespace Solarfield\Batten;
 
-class UnresolvedRouteException extends \Exception {
+use Exception;
 
+class UnresolvedRouteException extends \Exception {
+	private $routeInfo;
+
+	public function getRouteInfo() {
+		return $this->routeInfo;
+	}
+
+	public function __construct($message = null, $code = 0, Exception $previous = null, $aRouteInfo = null) {
+		parent::__construct($message, $code, $previous);
+		$this->routeInfo = $aRouteInfo;
+	}
 }
